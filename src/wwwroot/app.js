de1398a22dbc14a42c01f80fd04679da830f6039
Futupas <futupas@gmail.com>
2023-05-29 18:59:25 +0200

    add: song adding basic design

--- src/wwwroot/app.js
+++ src/wwwroot/app.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log('hi');
+document.getElementById('add-song-btn').onclick = e => {
+    document.getElementById('search').classList.remove('hidden');
+}
+
+document.querySelector('#search > .background').onclick = e => {
+    document.getElementById('search').classList.add('hidden');
+}
+
