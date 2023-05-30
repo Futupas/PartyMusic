f5ca93bde41a03c566837a32da05ea098670d9b0
Futupas <futupas@gmail.com>
2023-05-30 12:57:17 +0200

    uod: lil docker fix

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,5 @@
-# VLC
-# FROM galexrt/vlc:latest
-FROM jrottenberg/ffmpeg:4.1-alpine-vlc
-
-
 # Base image with .NET SDK 8 and VLC
 FROM mcr.microsoft.com/dotnet/sdk:7.0 AS base
-
-# Set an environment variable for VLC installation path
-ENV VLC_DIR /usr/lib/vlc
-
-# Install VLC dependencies
-# RUN apt update
-# RUN apt upgrade -y
-# RUN apt install vlc -y
 
 # Set the working directory
 WORKDIR /app
--- run_docker.sh
+++ run_docker.sh
@@ -5,3 +5,19 @@ docker build -t party-music:v1 .
 docker run -p 82:82 --device /dev/snd:/dev/snd -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix party-music:v1
 
 # docker exec <container_id> printenv VLC_DIR
+
+
+# MOUNT: 
+
+# docker run -d \
+#   -it \
+#   --name devtest \
+#   --mount type=bind,source="$(pwd)"/target,target=/app \
+#   nginx:latest
+
+#  docker run -d \
+#   -it \
+#   --name devtest \
+#   -v "$(pwd)"/target:/app \
+#   nginx:latest
+
